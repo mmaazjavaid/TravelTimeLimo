678368dccbc2aa52d9e90affef5c24ed5ecfd4c2
Danyal Nazir <danyalnazir10@gmail.com>
2025-01-20 23:25:01 +0500

    Added google places api for location searching

--- app/components/home/BookingForm.tsx
+++ app/components/home/BookingForm.tsx
@@ -7,9 +7,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useRouter } from 'next/navigation';
 import { Card, CardContent } from '@/components/ui/card';
 import { STYLES } from '@/lib/commonStyles';
+import Autocomplete from 'react-google-autocomplete';
 
 export function BookingForm() {
 	const router = useRouter();
+	const googleApiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY;
 
 	return (
 		<Card className="w-full max-w-2xl mx-auto bg-gradient-to-r from-gray-50 to-gray-100 shadow-2xl rounded-xl overflow-hidden">
@@ -34,32 +36,48 @@ export function BookingForm() {
 						<div className="space-y-6">
 							<div className="relative">
 								<MapPin className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
-								<Input
-									type="text"
+								<Autocomplete
+									apiKey={googleApiKey}
+									onPlaceSelected={place => {
+										console.log('Selected place:', place);
+									}}
+									types={['address']}
+									options={{
+										componentRestrictions: { country: 'us' },
+										region: 'new-york',
+									}}
 									placeholder="From: Address, airport, hotel..."
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<div className="relative">
 								<MapPin className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
-								<Input
-									type="text"
+								<Autocomplete
+									apiKey={googleApiKey}
+									onPlaceSelected={place => {
+										console.log('Selected place:', place);
+									}}
+									types={['address']}
+									options={{
+										componentRestrictions: { country: 'us' },
+										region: 'new-york',
+									}}
 									placeholder="To: Address, airport, hotel..."
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<div className="relative">
 								<Calendar className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
 								<Input
 									type="date"
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<div className="relative">
 								<Clock className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
 								<Input
 									type="time"
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<p className="text-sm text-gray-500 text-center">Chauffeur will wait 15 minutes free of charge.</p>
@@ -78,24 +96,32 @@ export function BookingForm() {
 						<div className="space-y-6">
 							<div className="relative">
 								<MapPin className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
-								<Input
-									type="text"
+								<Autocomplete
+									apiKey={googleApiKey}
+									onPlaceSelected={place => {
+										console.log('Selected place:', place);
+									}}
+									types={['address']}
+									options={{
+										componentRestrictions: { country: 'us' },
+										region: 'new-york',
+									}}
 									placeholder="Pickup location"
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<div className="relative">
 								<Calendar className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
 								<Input
 									type="date"
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<div className="relative">
 								<Clock className="absolute left-4 top-2 h-5 w-5 text-gray-400" />
 								<Input
 									type="time"
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full h-10 pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<div className="relative">
@@ -103,7 +129,7 @@ export function BookingForm() {
 								<Input
 									type="number"
 									placeholder="Number of hours"
-									className="w-full pl-12 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
+									className="w-full pl-12 h-10 bg-white border border-gray-300 rounded-lg shadow focus:border-indigo-500 focus:ring-indigo-500"
 								/>
 							</div>
 							<Button
