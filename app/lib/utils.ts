10d60e0bb64c70a78eb803d4c08d5a4c1af65d70
mmaazjavaid <mmaazj323@gmail.com>
2025-03-16 15:21:01 +0500

    Price increased

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -11,15 +11,29 @@ export function calculateFare(distance: number) {
   const fareBrackets = [
     { maxDistance: 9999, suv: 110, sedan: 85 },
     { maxDistance: 19999, suv: 120, sedan: 95 },
-    { maxDistance: 29999, suv: 130, sedan: 110 },
-    { maxDistance: 39999, suv: 150, sedan: 130 },
-    { maxDistance: 49999, suv: 170, sedan: 150 },
-    { maxDistance: 59999, suv: 190, sedan: 170 },
-    { maxDistance: 69999, suv: 210, sedan: 190 },
-    { maxDistance: 79999, suv: 230, sedan: 210 },
-    { maxDistance: 89999, suv: 250, sedan: 230 },
-    { maxDistance: 99999, suv: 270, sedan: 250 },
+    { maxDistance: 29999, suv: 150, sedan: 130 },
+    { maxDistance: 39999, suv: 170, sedan: 150 },
+    { maxDistance: 49999, suv: 190, sedan: 170 },
+    { maxDistance: 59999, suv: 210, sedan: 190 },
+    { maxDistance: 69999, suv: 230, sedan: 210 },
+    { maxDistance: 79999, suv: 250, sedan: 230 },
+    { maxDistance: 89999, suv: 270, sedan: 250 },
+    { maxDistance: 99999, suv: 290, sedan: 270 },
   ];
+
+
+  // const fareBrackets = [
+  //   { maxDistance: 9999, suv: 110, sedan: 85 },
+  //   { maxDistance: 19999, suv: 120, sedan: 95 },
+  //   { maxDistance: 29999, suv: 130, sedan: 110 },
+  //   { maxDistance: 39999, suv: 150, sedan: 130 },
+  //   { maxDistance: 49999, suv: 170, sedan: 150 },
+  //   { maxDistance: 59999, suv: 190, sedan: 170 },
+  //   { maxDistance: 69999, suv: 210, sedan: 190 },
+  //   { maxDistance: 79999, suv: 230, sedan: 210 },
+  //   { maxDistance: 89999, suv: 250, sedan: 230 },
+  //   { maxDistance: 99999, suv: 270, sedan: 250 },
+  // ];
 
   const fare = fareBrackets.find(bracket => distance <= bracket.maxDistance);
 
