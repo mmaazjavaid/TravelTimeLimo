5095824eb952dec84d316bd7cc73d37241a890e1
mmaazjavaid <mmaazj323@gmail.com>
2025-03-16 14:54:09 +0500

    image changed main

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import MainHeader from './components/home/MainHeader';
 export default function Home() {
 	return (
 		<>
-			<MainHeader heading={'Your global chauffeur service'} imagePath={'/car-12.jpg'} showDownloadSection={false} />
+			<MainHeader heading={'Your global chauffeur service'} imagePath={'/02-1024x576.jpg'} showDownloadSection={false} />
 			<ServicesSection />
 			<CityRoutes />
 			<FeaturesSection />
