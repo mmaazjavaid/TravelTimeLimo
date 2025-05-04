54f397cea688dbaaec19bb8af4e1909ac91d933d
mmaazjavaid <mmaazj323@gmail.com>
2025-05-04 19:44:28 +0500

    added final banner image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import MainHeader from './components/home/MainHeader';
 export default function Home() {
 	return (
 		<>
-			<MainHeader heading={'Your US chauffeur service'} imagePath={'/banner.jpg'} showDownloadSection={false} />
+			<MainHeader heading={'Your US chauffeur service'} imagePath={'/final-banner.jpg'} showDownloadSection={false} />
 			<ServicesSection />
 			<CityRoutes />
 			<FeaturesSection />
