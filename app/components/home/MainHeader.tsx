11f60893c48dff9523997ea61a099335ee2f11c9
mmaazjavaid <mmaazj323@gmail.com>
2025-05-04 15:36:11 +0500

    main header opacity fix

--- app/components/home/MainHeader.tsx
+++ app/components/home/MainHeader.tsx
@@ -10,11 +10,11 @@ export default function MainHeader({ heading, showDownloadSection = true, imageP
 			<section className="relative min-h-[50vh] md:min-h-[60vh] lg:min-h-[75vh] flex items-center">
 				<div className="absolute inset-0 z-0">
 					<img src={imagePath} alt="Luxury Car Service" className="w-full h-full object-cover" />
-					<div className="absolute inset-0 bg-black opacity-30"></div>
+					<div className="absolute inset-0 bg-black opacity-10"></div>
 				</div>
 				{showBookingDialog && (
 					<div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10 py-8 lg:py-12">
-						<div className="flex flex-col lg:flex-row lg:items-center lg:justify-end xl:relative xl:-top-[100px] xl:-right-[100px]">
+						<div className="flex flex-col lg:flex-row lg:items-center lg:justify-end">
 							<div>
 								<BookingForm />
 							</div>
